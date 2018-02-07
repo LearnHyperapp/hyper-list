9170dba7837bf2f048f93b51e1342a5f16b7b29f
Alex Barry <alex.barry@gmail.com>
2018-02-07 13:39:05 -0500

    Some documentation.

---
+++ README.md
@@ -0,0 +1,30 @@
+# Hyper List
+
+A simple hyperapp-powered list
+
+## Purpose
+
+Creates a minimal working todo-style list to demonstrate a minimum working hyperapp.
+
+This is one of my favourite basic apps because there are a lot of interesting upgrades we can demonstrate, like saving (via localstorage or third-party database), live collaboration (via websockets), live validation, lifecycle hooks, etc.
+I'll be using this repo to create many forks to demonstrate these various upgrades and step you, the reader, through those upgrades.
+
+## Getting started
+
+Install everything:
+
+```
+npm install
+# or
+yarn
+```
+
+Get it running:
+
+```
+npm start
+# or
+yarn start
+```
+
+[localhost:8080](http://localhost:8080)
--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import { h, app } from 'hyperapp'
 
-const state = {
-  items: [],
-  text: '',
+const initialState = {
+  items: [], // Will be a list of strings, each an item on the list
+  text: '', // Will be our memo of what text is in the input. May not actually be necessary, as we could allow the input to be *uncontrolled*.
 }
 
 const actions = {
-  addItem: item => state => ({ text: '', items: state.items.concat(item) }),
-  setText: value => state => ({ items: state.items, text: value }),
-  remItem: index => state => ({ items: state.items.slice(0, index).concat(state.items.slice(index + 1)) }),
+  addItem: item => state => ({
+    text: '', // Clear our input box
+    items: state.items.concat(item), // Add the item (also should be the same as state.text) to our items list
+  }),
+  setText: value => state => ({
+    text: value, // Update our text to `value`, which should be coming from oninput event.target.value (see line 46)
+  }),
+  remItem: index => state => ({
+    items: state.items
+      .slice(0, index) // Get all the items in the list before the index we're removing
+      .concat(state.items.slice(index + 1)) // Get all the items in the list after the index we're removing
+  }),
 }
 
 const view = (state, actions) => {
+  /*
+   * Some notes:
+   *  - Because we're outputting an array to the virtual dom, I'm keying all the <li>s.
+   *  - I'm also keying the <li> wrapping our input at the bottom to guarantee that hyperapp doesn't create a new dom element.
+   */
   return (
     <div>
       <ul>
@@ -26,7 +40,9 @@ const view = (state, actions) => {
             >
               &times;
             </a>
+
             &nbsp;
+
             {item}
           </li>
         ))}
@@ -36,9 +52,9 @@ const view = (state, actions) => {
             value={state.text}
             oninput={(e) => actions.setText(e.target.value)}
             onkeydown={(e) => {
-              if (e.which === 13) {
-                e.preventDefault()
-                actions.addItem(state.text)
+              if (e.which === 13) { // 13 is the enter/return key
+                e.preventDefault() // If we were in a form, we'd definitely need to call this to prevent a form submit. I think it's just a generally good idea for keyDown handling for enter in inputs.
+                actions.addItem(state.text) // Could also swap state.text with e.target.value if we had an uncontrolled input.
               }
             }}
           />
@@ -48,4 +64,5 @@ const view = (state, actions) => {
   )
 }
 
-const main = app(state, actions, view, document.getElementById('root'))
+// Mount the app
+const main = app(initialState, actions, view, document.getElementById('root'))
